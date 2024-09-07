5b7bcd23d496a4d49d265b930da8d92ab2207364
k9845 <khatririshi2430@gmail.com>
2024-09-07 06:37:37 +0545

    Add views for prodcuer

--- producer/views.py
+++ producer/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Producer, Customer, Product, Order, Sale
+from .serializers import ProducerSerializer, CustomerSerializer, ProductSerializer, OrderSerializer, SaleSerializer
+
+
+class ProducerViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing and editing producer instances.
+    """
+    queryset = Producer.objects.all()
+    serializer_class = ProducerSerializer
+
+
+class CustomerViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing and editing customer instances.
+    """
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing and editing product instances.
+    """
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing and editing order instances.
+    """
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+
+class SaleViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing and editing sale instances.
+    """
+    queryset = Sale.objects.all()
+    serializer_class = SaleSerializer
