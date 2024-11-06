946ae2513a22d66d59b8277f2624c1b87a2cfb74
k9845 <khatririshi2430@gmail.com>
2024-11-06 20:39:15 +0545

    Add location in the userprofile

--- user/serializers.py
+++ user/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from .models import UserProfile
 from django.contrib.auth.models import User
 
+from producer.models import City
+
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
@@ -10,10 +12,11 @@ class RegisterSerializer(serializers.ModelSerializer):
     last_name = serializers.CharField(required=True)
     phone_number = serializers.CharField(required=True)
     email = serializers.EmailField(required=True)
+    location = serializers.PrimaryKeyRelatedField(queryset=City.objects.all(), required=True)
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password', 'password2', 'first_name', 'last_name', 'phone_number']
+        fields = ['username', 'email', 'password', 'password2', 'first_name', 'last_name', 'phone_number', 'location']
 
     def validate(self, attrs):
         if attrs['password'] != attrs['password2']:
@@ -22,6 +25,7 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         phone_number = validated_data.pop('phone_number')
+        location = validated_data.pop('location')
         user = User.objects.create(
             username=validated_data['username'],
             email=validated_data['email'],
@@ -34,6 +38,7 @@ class RegisterSerializer(serializers.ModelSerializer):
         # Create UserProfile instance
         UserProfile.objects.create(
             user=user,
-            phone_number=phone_number
+            phone_number=phone_number,
+            location=location
         )
         return user
