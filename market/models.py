bd6508759eefc67d73b16acedd2fb55daba20ff3
k9845 <khatririshi2430@gmail.com>
2025-06-25 12:36:27 +0545

    Update delivery models

--- market/models.py
+++ market/models.py
@@ -257,7 +257,7 @@ class CartItem(models.Model):
 
 
 class Delivery(models.Model):
-    cart = models.OneToOneField(Cart, on_delete=models.CASCADE, related_name="delivery")
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name="delivery")
     customer_name = models.CharField(max_length=255)
     phone_number = models.CharField(max_length=20)
     address = models.TextField()
