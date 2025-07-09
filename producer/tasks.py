7b9952f6bdad977543f596b374aaad6747c5b39c
k9845 <khatririshi2430@gmail.com>
2025-07-09 11:34:44 +0545

    Fix celery error

--- producer/tasks.py
+++ producer/tasks.py
@@ -25,24 +25,24 @@ def move_large_stock_to_stocklist():
     return f"{len(products)} products checked."
 
 
-def get_bids_last_hour(product):
-    one_hour_ago = timezone.now() - timedelta(hours=1)
-    return Bid.objects.filter(product=product, bid_date__gte=one_hour_ago).count()
+# def get_bids_last_hour(product):
+#     one_hour_ago = timezone.now() - timedelta(hours=1)
+#     return Bid.objects.filter(product=product, bid_date__gte=one_hour_ago).count()
 
 
-def get_bids_last_day(product):
-    one_day_ago = timezone.now() - timedelta(days=1)
-    return Bid.objects.filter(product=product, bid_date__gte=one_day_ago).count()
+# def get_bids_last_day(product):
+#     one_day_ago = timezone.now() - timedelta(days=1)
+#     return Bid.objects.filter(product=product, bid_date__gte=one_day_ago).count()
 
 
-@shared_task
-def update_bid_end_dates():
-    products = MarketplaceProduct.objects.filter(is_available=True)
+# @shared_task
+# def update_bid_end_dates():
+#     products = MarketplaceProduct.objects.filter(is_available=True)
 
-    for product in products:
-        bids_last_hour = get_bids_last_hour(product)
-        bids_last_day = get_bids_last_day(product)
-        product.update_bid_end_date(bids_last_hour, bids_last_day)
+#     for product in products:
+#         bids_last_hour = get_bids_last_hour(product)
+#         bids_last_day = get_bids_last_day(product)
+#         product.update_bid_end_date(bids_last_hour, bids_last_day)
 
 
 SERVICE_LEVEL_Z = 1.65
