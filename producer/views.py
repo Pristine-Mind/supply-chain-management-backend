b3ec3333d3f0e0e2e4f4847ae17d039c76eaa0dd
k9845 <khatririshi2430@gmail.com>
2025-07-21 06:38:35 +0545

    Add missing business_type

--- producer/views.py
+++ producer/views.py
@@ -665,6 +665,7 @@ class UserInfoView(APIView):
                 "username": user.username,
                 "id": user.id,
                 "has_access_to_marketplace": has_access_to_marketplace,
+                "business_type": user_profile.business_type,
             }
         )
 
