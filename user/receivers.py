f730e633b5d4f59d95ea1be224c82521edd5c9fe
k9845 <khatririshi2430@gmail.com>
2025-11-10 06:33:07 +0545

    Fix IntegrityError

--- user/receivers.py
+++ user/receivers.py
@@ -1,6 +1,7 @@
 import uuid
 
 from django.contrib.auth.models import User
+from django.db import IntegrityError
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
@@ -13,9 +14,10 @@ def create_user_profile(sender, instance, created, **kwargs):
         # Get the general_user role
         general_user_role = Role.objects.get(code="general_user")
 
-        # Create UserProfile for all new users with general_user role
+        # Create UserProfile for all new users with general_user role.
+        # Use get_or_create to avoid duplicate creation when an admin inline
+        # or another piece of code created the profile earlier in the request.
         profile_data = {
-            "user": instance,
             "role": general_user_role,
         }
 
@@ -23,7 +25,12 @@ def create_user_profile(sender, instance, created, **kwargs):
         if instance.is_superuser:
             profile_data["shop_id"] = str(uuid.uuid4())
 
-        _ = UserProfile.objects.create(**profile_data)
+        try:
+            UserProfile.objects.get_or_create(user=instance, defaults=profile_data)
+        except IntegrityError:
+            # In case of a race where a profile was created concurrently
+            # (e.g., admin inline + post_save), ignore the error.
+            pass
 
 
 # @receiver(post_save, sender=User)
